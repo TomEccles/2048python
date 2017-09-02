7906967922920237d2f444ce3116426641e96f10
Tom Eccles <eccles.tom@gmail.com>
2017-09-02 07:44:21 +0100

    Add functions to run policy training.

--- python/analyser.py
+++ python/analyser.py
@@ -15,7 +15,8 @@ def mean_std(array):
 
 class Analyser:
     def __init__(self, output_dir):
-        results_files = [output_dir + "/" + s for s in os.listdir(output_dir)]
+        results_files = [output_dir + "/" + s for s in os.listdir(output_dir) if s.startswith("result")]
+        #results_files = [output_dir + "/" + s for s in os.listdir(output_dir)]
         self.results = []
         for filename in results_files:
             with open(filename, "r") as file:
@@ -28,5 +29,14 @@ class Analyser:
             print((name, len(turns), mean(turns), max(turns), mean_std(turns)))
 
 
-a = Analyser("./results/runs/test")
+a = Analyser("./results/runs/run1504073134/output")
+
 a.get_stats()
+#
+# file_name = "./results/runs/policy_test/results.txt"
+# block = 2000
+# with open(file_name, "r") as file:
+#     turns = [int(line.split(" ")[0]) for line in file.readlines()]
+#     for i in range(len(turns) // block):
+#         a = turns[i*block : (i+1)*block]
+#         print(i*block, (i+1)*block - 1, mean(a), max(a), mean_std(a))
--- python/game_player.py
+++ python/game_player.py
@@ -5,11 +5,12 @@ from board import Board, all_moves
 from file_utils import open_creating_dir_if_needed
 from move_player import MovePlayer
 from nets import Nets
+from policy_player import PolicyPlayer
 
 runs_root = "./results/runs"
 vanilla_data_path = runs_root + "/vanilla/output/data"
 
-def play_game(evals, nets):
+def play_game(get_move):
     to_move_boards = []
     to_appear_boards = []
     labels = []
@@ -18,7 +19,7 @@ def play_game(evals, nets):
     start = time.time()
     while b.can_add_random():
         b = b.add_random()
-        updated_board = MovePlayer(b, nets).play(evals)
+        updated_board = get_move(b)
         if updated_board is None:
             break
 
@@ -35,24 +36,37 @@ def play_game(evals, nets):
     return moves, time.time() - start, to_move_boards, labels, to_appear_boards
 
 
-def get_data(games, nets, evals, results_file=None):
+def values(moves):
+    r = 0.99
+    return [1-r**(moves - i) for i in range(moves)]
+
+
+def get_data(games, get_move, results_file=None):
     """
     Plays some games of 2048, and returns everything that happened
     """
     to_move_boards = []
     to_appear_boards = []
-    l = []
-    r = []
+    labels = []
+    results = []
+    move_scores = []
     for _ in range(games):
-        moves, t, boards, labels, a_boards = play_game(evals, nets)
+        score, t, boards, moves, a_boards = play_game(get_move)
         to_move_boards = to_move_boards + boards
         to_appear_boards = to_appear_boards + a_boards
-        l = l + labels
-        r.append(moves)
+        labels += moves
+        results.append(score)
+        move_scores += values(score)
         if results_file:
             with open_creating_dir_if_needed(results_file, 'a') as file:
-                file.write("%d %f\n" % (moves, t))
-    return to_move_boards, l, to_appear_boards, r
+                file.write("%d %f\n" % (score, t))
+    return to_move_boards, labels, to_appear_boards, results, move_scores
+
+
+def get_move_mcts(nets, evals):
+    def move(b):
+        return MovePlayer(b, nets).play(evals)
+    return move
 
 
 def train(nets, game_batch, passes, evals, valid_batch, path, runs=-1):
@@ -70,9 +84,10 @@ def train(nets, game_batch, passes, evals, valid_batch, path, runs=-1):
     count = 0
     while count != runs:
         count += 1
-        boards, labels, a_boards, results = get_data(game_batch, nets, evals,
+        boards, labels, a_boards, results, _ = get_data(game_batch,
+                                                     get_move_mcts(nets, evals),
                                                      path + "/output/results_iter_%i.txt" % count)
-        b_val, l_val, a_val, r_val = get_data(valid_batch, nets, evals)
+        b_val, l_val, a_val, r_val, _ = get_data(valid_batch, get_move_mcts(nets, evals))
         nets.feed_observations(boards, labels, a_boards, passes)
         nets.validate_observations(b_val, l_val, a_val)
         nets.save(path + "/checkpoints/predict_iter_%i.ckpt" % count, path + "/checkpoints/value_iter_%i.ckpt" % count)
@@ -86,8 +101,9 @@ def get_vanilla_data(evals, games):
     training variations of networks on.
     """
     nets = Nets(0, 0)
-    boards, labels, a_boards, results = get_data(games, nets, evals, runs_root + "/vanilla/output/results.txt")
-    with open_creating_dir_if_needed(vanilla_data_path, "w") as file:
+
+    boards, labels, a_boards, results = get_data(games, get_move_mcts(nets, evals), runs_root + "/vanilla/output/results.txt")
+    with open_creating_dir_if_needed(vanilla_data_path, "wb") as file:
         pickle.dump([boards, labels, a_boards, results], file)
 
 
@@ -105,22 +121,23 @@ def load_vanilla_data_and_train():
     d, l, a, r = pickle_load(vanilla_data_path)
     print(len(d), len(l), len(a), len(r), d[0], l[0], a[0], r[0])
     nets = Nets(0, 0, None, None)
-    feed_obs = 10000
-    valid_obs = 1000
+    feed_obs = 100000
+    valid_obs = 20000
 
     end = feed_obs + valid_obs
 
     if end > min(len(d), len(l), len(a)):
         raise ValueError("Not enough data")
 
-    nets.feed_observations(d[:feed_obs], l[:feed_obs], a[:feed_obs], 10)
-    nets.validate_observations(d[feed_obs:feed_obs], l[feed_obs:end], a[feed_obs:end])
+    while True:
+        nets.feed_observations(d[:feed_obs], l[:feed_obs], a[:feed_obs], 10)
+        nets.validate_observations(d[feed_obs:end], l[feed_obs:end], a[feed_obs:end])
 
 
-def train_from_vanilla():
+def train_from_vanilla(game_batch, evals, passes, valid_batch):
     run_path = runs_root + "/run%.0f" % time.time()
     nets = Nets(100, 10)
-    train(nets, game_batch=100, evals=100, passes=5, valid_batch=10, path=run_path, runs=-1)
+    train(nets, game_batch, passes, evals, valid_batch, run_path)
 
 
 def test_outputs():
@@ -129,7 +146,31 @@ def test_outputs():
     evals = 100
     while True:
         nets = Nets(100, 10, load_path + "predict_iter_11.ckpt", load_path + "value_iter_11.ckpt")
-        get_data(10, nets, evals, run_path + "last_iter.txt")
+        get_data(10, get_move_mcts(nets, evals), run_path + "last_iter.txt")
+
+
+def normalise(res):
+    m = sum(res) / len(res)
+    std = (sum([(r-m)**2 for r in res])/len(res)) ** 0.5
+    return [(r-m) / std for r in res]
+
+
+def train_policy(game_batch, passes, runs):
+
+    load_path = runs_root + "/run1499671533/checkpoints/" + "predict_iter_1.ckpt"
+
+    trainer = PolicyPlayer(load_path)
+    def get_move_policy(b):
+        return b.move(trainer.get_move(b))
+    r = 0
+    while r != runs:
+        to_move_boards, l, _, _, res = get_data(game_batch, get_move_policy, runs_root + "/policy_test/results.txt")
+        r += 1
+        res = normalise(res)
+        trainer.feed_observations(to_move_boards, l, res, passes)
+
 
 # cProfile.run('run_one_game()')
-train_from_vanilla()
+#get_vanilla_data(100, 100)
+#train_policy(game_batch=100, passes=10, runs=-1)
+train_from_vanilla(100, 100, 10, 10)
